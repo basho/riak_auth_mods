9d52a51a8973b061dfe4a104f7f6736ae47ddd99
Andrew Thompson <andrew@hijacked.us>
2013-07-17 17:57:50 -0400

    Initial import

---
+++ src/riak_auth_mods_pam.erl
@@ -0,0 +1,11 @@
+-module(riak_auth_mods_pam).
+
+-export([auth/4]).
+
+auth(Username, Password, _UserOptions, SourceOptions) ->
+    P = canola:open(),
+    Service = proplists:get_value(service, SourceOptions, "riak"),
+    Res = canola:auth(list_to_binary(Username), list_to_binary(Password),
+                list_to_binary(Service), P),
+    canola:close(P),
+    Res.
