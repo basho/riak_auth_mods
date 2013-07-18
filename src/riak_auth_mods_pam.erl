5f079ddd263677364bf8ecde0b9684b086250c4d
Andrew Thompson <andrew@hijacked.us>
2013-07-18 16:01:11 -0400

    Pull right option out of proplist for pam service

--- src/riak_auth_mods_pam.erl
+++ src/riak_auth_mods_pam.erl
@@ -4,7 +4,7 @@
 
 auth(Username, Password, _UserOptions, SourceOptions) ->
     P = canola:open(),
-    Service = proplists:get_value(service, SourceOptions, "riak"),
+    Service = proplists:get_value("service", SourceOptions, "riak"),
     Res = canola:auth(list_to_binary(Username), list_to_binary(Password),
                 list_to_binary(Service), P),
     canola:close(P),
